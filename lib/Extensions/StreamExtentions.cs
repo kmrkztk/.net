f0534ca18cdb9d7bf0f037506ceae3abcc72f193
Kazutaka Kimura <kazutaka_kimura@integratto.co.jp>
2022-01-18 16:51:14 +0900

    tail

---
+++ lib/Extensions/StreamExtentions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public static class StreamExtentions
+    {
+        public static void SeekTo(this Stream stream, SeekOrigin origin, byte c, int count = 1)
+        {
+            const int BUFFER_SIZE = 32;
+            stream.Seek(0, origin);
+            var len = stream.Length;
+            var pos = stream.Position;
+            var buf = new byte[BUFFER_SIZE];
+            var cnt = 0;
+
+            // 末尾から
+            if (origin == SeekOrigin.End)
+            {
+                while(pos > 0)
+                {
+                    var l = pos - BUFFER_SIZE > 0 ? BUFFER_SIZE : (int)pos;
+                    stream.Seek(-l, SeekOrigin.Current);
+                    stream.Read(buf, 0, l);
+                    stream.Seek(-l, SeekOrigin.Current);
+                    pos -= l;
+                    for (var i = l - 1; i >= 0; i--)
+                    {
+                        if (buf[i] != c) continue;
+                        if (++cnt >= count)
+                        {
+                            stream.Seek(i + 1, SeekOrigin.Current);
+                            return;
+                        }
+                    }
+                }
+            }
+            // 先頭、もしくは現在位置から
+            else
+            {
+                while (pos < len)
+                {
+                    var l = pos + BUFFER_SIZE < len ? BUFFER_SIZE : (int)(len - pos);
+                    stream.Read(buf, 0, l);
+                    pos += l;
+                    for (var i = 0; i < l; i++)
+                    {
+                        if (buf[i] != c) continue;
+                        if (++cnt >= count)
+                        {
+                            stream.Seek(i - l, SeekOrigin.Current);
+                            return;
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ tail/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+
+namespace tail
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = FileArguments<Options>.Load();
+            foreach (var reader in a.GetReaders().Select(_ => _ as StreamReader))
+            {
+                var stream = reader.BaseStream;
+                if (stream.Length == 0) return;
+
+                // 末尾が改行している場合はカウントしないように
+                stream.Seek(-1, SeekOrigin.End);
+                var last = stream.ReadByte();
+                var count = a.Options.Count;
+                if (last == 0x0a) count++;
+
+                stream.SeekTo(SeekOrigin.End, 0x0a, count);
+                Console.Write(reader.ReadToEnd());
+
+                var pos = stream.Position;
+                while (a.Options.Follow)
+                {
+                    System.Threading.Thread.Sleep(500);
+                    if (pos > stream.Length)
+                    {
+                        stream.Position = stream.Length;
+                        pos = stream.Position;
+                    }
+                    if (pos == stream.Length) continue;
+                    Console.Write(reader.ReadToEnd());
+                    pos = stream.Position;
+                }
+                break;
+            }
+        }
+
+        [Detail("tail file [/options]")]
+        class Options
+        {
+            [Command] [Command("n")] [CommandValue] public int Count { get; set; } = 10;
+            [Command] [Command("f")] public bool Follow { get; set; } = false;
+        }
+    }
+}
